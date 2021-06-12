fd049f73ca3df7763fa8c4bfc3ac236dd1d74dd9
hliosdja <68526992+hliosdja@users.noreply.github.com>
2021-06-12 12:03:53 +0800

    Initial setup

---
+++ src/components/player.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Player = () => {
+    return(
+        <div className="player-container">
+            <div className="time-control">
+                <p>Start time</p>
+                <input type="range" name="" id=""/>
+                <p>End time</p>
+            </div>
+            <div className="player-control">
+
+            </div>
+        </div>
+    )
+}
+
+export default Player;
---
+++ src/components/song.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Song = () => {
+    return(
+        <div className="song-container">
+            <h1>Song Art</h1>
+            <h1>Song Title</h1>
+            <h1>Artist</h1>
+        </div>
+    )
+}
+
+export default Song;
